f0fca06a1cd6ad9c69de9b139ff11c1a0359bc34
leihc <leihcsky@163.com>
2026-04-28 13:44:04 +0800

    配置修改

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Workaround for Windows EPERM on ".next/trace" by using a fresh build directory.
-  distDir: "next-dev",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
